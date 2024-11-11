f23ff1c2b71eacbcd606397544a94708a315d6e2
Xinrui Ding <dingx14@cardiff.ac.uk>
2024-11-11 19:47:14 +0000

    now can edit, save and show updated name use two way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,36 @@
 import { useState } from "react"
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
   const [isEditing, setIsEditing] = useState(false)
+  const [playerName, setPlayerName] = useState(initialName)
 
   function handleEditClick() {
-    setIsEditing(true)
+    setIsEditing(Editing => !Editing) //注意这是函数内部的参数，和外面的那个isEditing不是一个东西！
+    // 只不过这个函数会被react识别然后由它来更新状态
+    // 不能写setIsEditing(!isEditing)
     // console.log('isClicked');
     
   }
 
-  let playerName = <span className="player-name">{name}</span>
+  function handleChange(event) {
+    setPlayerName(event.target.value)
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>
+  // let btnCaption = 'Edit'
 
   if (isEditing) {
-    playerName = <input type="text" required/>
+    editablePlayerName = <input type="text" required defaultValue={playerName} onChange={handleChange}/>
+    // btnCaption= 'Save'
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
     )
 }
