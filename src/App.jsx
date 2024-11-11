a32d6bd94107e0ae6c9e5b70c4d682be5ad7dc9d
Xinrui Ding <dingx14@cardiff.ac.uk>
2024-11-11 20:04:10 +0000

    init game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 
 function App() {
   
@@ -10,6 +11,8 @@ function App() {
           <Player initialName='Player 1' symbol='X'/>
           <Player initialName='Player 2' symbol='O'/>          
         </ol>
+
+        <GameBoard />
       </div>
       LOG
     </main>
