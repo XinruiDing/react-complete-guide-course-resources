36f7c1525d01474e10532c422da9853123996ab3
Xinrui Ding <dingx14@cardiff.ac.uk>
2024-11-13 19:23:22 +0000

    add restart logic

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   
   const activePlayer = deriveActivePlayer(gameTurns)
 
-  let gameBoard = initialGameBoard
+  let gameBoard = [...initialGameBoard.map(innerArr => [...innerArr])]
 
   for (const turn of gameTurns) {
     const { square, player } = turn
@@ -70,6 +70,10 @@ function App() {
     })
   }
 
+  function handleRestart() {
+    setGameturns([])
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -77,7 +81,7 @@ function App() {
           <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'}/>
           <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'}/>          
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner}/>}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard 
           onSelectSquare={handleSelectSquare}
           board={gameBoard}
